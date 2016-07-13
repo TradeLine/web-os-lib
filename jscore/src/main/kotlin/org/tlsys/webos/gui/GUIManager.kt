f2a98c3bc32637f4ac782931eb1be3186dd2562a
Bird <mail@birdlab.ru>
2016-07-14 01:03:18 +0300

    Добавил методы получения размера рабочего пространства

--- jscore/src/main/kotlin/org/tlsys/webos/gui/GUIManager.kt
+++ jscore/src/main/kotlin/org/tlsys/webos/gui/GUIManager.kt
@@ -4,4 +4,6 @@ interface GUIManager {
     val applications: List<GUIApplication<GUIApplicationController>>
     fun <C : GUIApplicationController> createApplication(controllerProvider: (GUIApplication<GUIApplicationController>) -> C): GUIApplication<C>
     fun activateWindow(window:GUIWindow<*,*>)
+    val screenWidth:Int
+    val screenHeight:Int
 }
