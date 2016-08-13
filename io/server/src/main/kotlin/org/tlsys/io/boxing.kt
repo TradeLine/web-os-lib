c89694db3acd88d7a1451dcf24c1e49ba8a11f75
Subochev <caffeine.mgn@gmail.com>
2016-08-13 15:03:52 +0300

    Добавил разгроничение видимости покупателей для разных компаний. Добавил новые права доступа. Привязал проверку прав доступа при вызове некоторых функций

---
+++ io/server/src/main/kotlin/org/tlsys/io/boxing.kt
@@ -0,0 +1,83 @@
+package org.tlsys.io
+
+import java.lang.reflect.Method
+import java.util.*
+
+private fun <T> unboxCast(valueFrom: DTO?, to: Class<*>): T? {
+    if (valueFrom === null)
+        return null
+    if (valueFrom.javaClass === to)
+        return valueFrom as T
+
+    when (to) {
+        String::class.java -> return (valueFrom as StringDTO).value as T
+        Boolean::class.java -> return (valueFrom as BooleanDTO).value as T
+        Char::class.java -> return (valueFrom as CharDTO).value as T
+        Byte::class.java -> return (valueFrom as ByteDTO).value as T
+        Short::class.java -> return (valueFrom as ShortDTO).value as T
+        Int::class.java -> return (valueFrom as IntDTO).value as T
+        Float::class.java -> return (valueFrom as FloatDTO).value as T
+        Long::class.java -> return (valueFrom as LongDTO).value as T
+        Double::class.java -> return (valueFrom as DoubleDTO).value as T
+    }
+
+    when(valueFrom) {
+        is StringDTO->return valueFrom.value as T
+        is BooleanDTO->return valueFrom.value as T
+        is CharDTO->return valueFrom.value as T
+        is ByteDTO->return valueFrom.value as T
+        is ShortDTO->return valueFrom.value as T
+        is IntDTO->return valueFrom.value as T
+        is FloatDTO->return valueFrom.value as T
+        is LongDTO->return valueFrom.value as T
+        is DoubleDTO->return valueFrom.value as T
+    }
+
+    TODO("Can't convert ${valueFrom!!.javaClass.name} to ${to.name}")
+}
+
+private fun boxCast(valueFrom: Any?): DTO? {
+    if (valueFrom === null)
+        return null
+    if (valueFrom is DTO)
+        return valueFrom
+    when (valueFrom) {
+        is String -> return StringDTO(valueFrom)
+        is Boolean -> return BooleanDTO(valueFrom)
+        is Char -> return CharDTO(valueFrom)
+        is Byte -> return ByteDTO(valueFrom)
+        is Short -> return ShortDTO(valueFrom)
+        is Int -> return IntDTO(valueFrom)
+        is Long -> return LongDTO(valueFrom)
+        is Float -> return FloatDTO(valueFrom)
+        is Double -> return DoubleDTO(valueFrom)
+        is DTO -> return valueFrom
+    }
+
+    if (valueFrom.javaClass.isArray) {
+        var len = java.lang.reflect.Array.getLength(valueFrom)
+        val out = ArrayList<DTO?>()
+        for (i in 0..len - 1)
+            out.add(boxCast(java.lang.reflect.Array.get(valueFrom, i)))
+        return  ListDTO<DTO>(out)
+    }
+
+    if (valueFrom is List<*>) {
+        val out = ArrayList<DTO?>(valueFrom.size)
+        for (i in valueFrom) {
+            out.add(boxCast(i))
+        }
+        return ListDTO<DTO>(out)
+    }
+
+    throw RuntimeException("Can't convert ${valueFrom.javaClass.name} to DTO")
+}
+
+fun Any.toDTO() = boxCast(this)
+fun <T>DTO.toValue(clazz:Class<T>):T? = unboxCast(this, clazz)
+
+fun Method.invoke(self:Any?, vararg arguments:DTO?){
+    if (parameterCount != arguments.size)
+        throw IllegalArgumentException("Can't Invoke method: bad arguments length")
+    this.invoke(self, *Array(parameterCount, { arguments[it]?.toValue(parameterTypes[it]) }))
+}
